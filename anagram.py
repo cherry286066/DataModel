e16c589765faa27df581fde06891bb9c8d84dd5b
Sai Charan Peddigari <saicharan7386@gmail.com>
2023-05-30 23:33:58 +0530

    anagram done

---
+++ anagram.py
@@ -0,0 +1,12 @@
+x=input("enter a string")
+y=input("enter another string")
+s=0
+for i in range(len(x)):
+    for j in range(len(y)):
+        if x[i]==y[j]:
+            s=s+1
+            break
+if s==len(x):
+    print("it is anagram")
+else:
+    print("its not an anagram")
